7bf4306f48139769557a4cb2f4484143e88b3f49
Neville <neville.lewis@>
2020-08-29 14:31:41 -0600

    trigger 6

--- src/app.py
+++ src/app.py
@@ -9,6 +9,6 @@ def lambda_handler(event, context):
         "statusCode": 200,
         "body": json.dumps({
             "message": "hello world -I made a change, which doesn't trigger - or does it now?.",
-             "location": ip.text.replace("\n", "")
+             #"location": ip.text.replace("\n", "")
         }),
     }
