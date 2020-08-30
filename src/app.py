abd2f8d4db3006bc0209a64a48938f34c5eb9cd2
Neville <neville.lewis@>
2020-08-29 21:48:48 -0600

    trigger 10

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello world - trigger 9.",
+            "message": "hello world - trigger 10.",
              #"location": ip.text.replace("\n", "")
         }),
     }
